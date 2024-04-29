783390b6d128efbe9f0b32759d1c856a9018dbc0
Michael Hernando <mhernando334@gmail.com>
2024-04-29 13:48:08 -0400

    data initialization

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,24 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+import data from './data/tips.csv'
 
 function App() {
+  const [csvData, setCsvData] = useState([]);
+
+  useEffect(() => {
+    d3.csv(data)
+      .then((fetchedData) => {
+        setCsvData(fetchedData);
+      })
+      .catch((error) => {
+        console.error("Error loading the csv file at path");
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(csvData)
+  }, [csvData])
+
   return (
     <div className="App">
     </div>
